645f5fa0d1864302190b3b9da52c46960f209fe3
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-15 09:45:45 -0600

    i broke it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-const sayHello = () => {console.log("Hello")};
-sayHello();
 
-const $ = require('jquery');
 
+import $ from 'jquery';
+// const {item, thing} = require('./practice.js');
+import cart from './practice'
+// import {sayHello} from './say-hello.js'
 $('body').html('What').css('color', 'red');
-
+const $ = require('jquery');
+const req = require('./say-hello.js');
+// console.log(item.color);
+// console.log(thing.color);
+// console.log(sayHello());
+req();
+console.log(cart.item.color);
---
+++ src/practice.js
@@ -0,0 +1,18 @@
+const item = {
+    name: "eggs",
+    weight: "eggs",
+    type: "eggs",
+    color: "brown"
+};
+
+
+const thing = {
+    name: "milk",
+    weight: "more than eggs",
+    type: "dairy",
+    color: "lavender"
+};
+
+// module.exports = {item, thing};
+
+export default {item, thing};
